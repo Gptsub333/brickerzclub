5dd04fb2be0ad00ea179b92da6d0c3ed73e0bc1a
om-chikhaliya <om.chikhaliya@holbox.ai>
2025-01-29 10:32:03 +0530

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/md', 'react-icons/lu', 'react-icons/sl'],  // Avoid externalizing this package if needed
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['react-icons/md', 'react-icons/lu', 'react-icons/sl'],  // Avoid externalizing this package if needed
+  //   }
+  // }
+  optimizeDeps: { include: ['react-icons'] }
 })
